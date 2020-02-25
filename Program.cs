4b0965de72e1d9266acd07b9508f5482a0544062
Avishek Patra <avishek.patra@lsz.ads.valuelabs.net>
2020-02-25 17:46:02 +0530

    Implementation of Sieve_of_Eratosthenes

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace sieveoferatosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new SieveOfEratosthenes();
+            //Calling the Generate Sieve Method to find out the result.
+            var results = obj.GenerateSieve(10);
+            foreach (var item in results)
+            {
+                //Outputting iin the console.
+                Console.WriteLine(item + " ");
+            }
+        }
+    }
+
+    public class SieveOfEratosthenes
+    {
+        // Build a Sieve of Eratosthenes.
+        public bool[] GenerateSieve(int max)
+        {
+            // Creating an array indicating whether numbers are prime.
+            bool[] is_prime = new bool[max + 1];
+            for (int i = 2; i <= max; i++) is_prime[i] = true;
+
+            // Crossing out multiples.
+            for (int i = 2; i <= max; i++)
+            {
+                // Check if i is prime.
+                if (is_prime[i])
+                {
+                    // Eliminate multiples of i.
+                    for (int j = i * 2; j <= max; j += i)
+                        is_prime[j] = false;
+                }
+            }
+            return is_prime;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # sieveoferatosthenes
-Implementation of Sieve of Eratosthenes
+Implementation of Sieve of Eratosthenes to find all the prime numbers from a large set of number, the alogoritm has been tested on various conditions to evaluate the performance
+
+Algorithm Reference:
+https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+
+Sample output when tested for 10 set of data
+
+False - 0
+False - 1
+True - 2
+True - 3
+False - 4
+True - 5
+False - 6
+True - 7
+False - 8
+False - 9
+False - 10
